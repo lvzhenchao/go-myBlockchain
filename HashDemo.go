cfb26606311ca7934cf31f0c5dd6307ef124ac79
lvzhenchao <1370164516@qq.com>
2019-06-18 18:35:20 +0800

    hashDemo测试算法

---
+++ HashDemo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+)
+
+func calculateHash(toBeHashed string) string{
+	hashInBytes := sha256.Sum256([]byte(toBeHashed))//哈希运算，转成字节切片，返回的是字节数组
+	hashInStr   := hex.EncodeToString(hashInBytes[:])//转成字符串
+
+	log.Printf("%s, %s", toBeHashed, hashInStr)//记录一下日志
+	return hashInStr
+
+}
+
+func main() {
+	calculateHash("test1")
+	calculateHash("test1")
+	calculateHash("testl")
+}
