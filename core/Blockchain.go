5c539718dbc6a08ee733f8c7cdd9764d135d57df
lvzhenchao <1370164516@qq.com>
2019-06-24 17:52:18 +0800

    Update Blockchain.go

--- core/Blockchain.go
+++ core/Blockchain.go
@@ -16,12 +16,14 @@ func NewBlockchain() *Blockchain{
 	return &blockchain
 }
 
+//发送数据
 func (bc *Blockchain) SendData(data string) {
 	preBlock := bc.Blocks[len(bc.Blocks) -1]
 	newBlock := GenerateNewBlock(*preBlock, data)
 	bc.ApendBlock(&newBlock)
 }
 
+//先往区块添加数据
 func (bc *Blockchain) ApendBlock(newBlock *Block){
 	if len(bc.Blocks) == 0 {
 		bc.Blocks = append(bc.Blocks, newBlock)
@@ -35,6 +37,7 @@ func (bc *Blockchain) ApendBlock(newBlock *Block){
 
 }
 
+//打印
 func(bc *Blockchain) Print(){
 	for _, block := range bc.Blocks {
 		fmt.Printf("Index： %d\n", block.Index)
@@ -46,11 +49,12 @@ func(bc *Blockchain) Print(){
 	}
 }
 
-func isValid(newBlock Block, oldBlock Block) bool{
+//验证
+func isValid(newBlock Block, oldBlock Block) bool {
 	if newBlock.Index - 1 != oldBlock.Index{
 		return false
 	}
-	if newBlock.PrevBlockHash != newBlock.Hash{
+	if newBlock.PrevBlockHash != oldBlock.Hash{
 		return false
 	}
 	if calculateHash(newBlock) != newBlock.Hash {
